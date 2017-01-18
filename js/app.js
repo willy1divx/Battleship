6458190cd9b83db9bf4cd559d3a11cc34d4b2717
williamd <willy1divx@yahoo.com>
2017-01-18 17:40:26 +0000

    added better randomization and unbinded click

--- js/app.js
+++ js/app.js
@@ -11,96 +11,112 @@ function increaseCount(count) {
 	}
 
 	$('.1').click(function(){
+			$('.1').unbind("click");
 				var number = 1;
 				checkHit(number);
 			$(this).attr("src", image);
 				ifSank();
 				});
 	$('.2').click(function(){
+			$('.2').unbind("click");
 				var number = 2;
 				checkHit(number);
 			$(this).attr("src", image);
 				ifSank();
 				});
 	$('.3').click(function(){
+			$('.3').unbind("click");
 				var number = 3;
 				checkHit(number);
 			$(this).attr("src", image);
 				ifSank();
 				});
 	$('.4').click(function(){
+			$('.4').unbind("click");
 				var number = 4;
 				checkHit(number);
 			$(this).attr("src", image);
 				ifSank();
 				});
 	$('.5').click(function(){
+			$('.5').unbind("click");
 			var number = 5;
 				checkHit(number);
 			$(this).attr("src", image);
 				ifSank();
 				});
 	$('.6').click(function(){
+			$('.6').unbind("click");
 				var number = 6;
 				checkHit(number);
 			$(this).attr("src", image);
 				ifSank();
 				});
 	$('.7').click(function(){
+			$('.7').unbind("click");
 				var number = 7;
 				checkHit(number);
 			$(this).attr("src", image);
 				ifSank();
 				});
 	$('.8').click(function(){
+			$('.8').unbind("click");
 				var number = 8;
 				checkHit(number);
 			$(this).attr("src", image);
 				ifSank();
 				});
 	$('.9').click(function(){
+			$('.9').unbind("click");
 				var number = 9;
 				checkHit(number);
 			$(this).attr("src", image);
 				ifSank();
 				});
 	$('.10').click(function(){
+			$('.10').unbind("click");
 				var number = 10;
 				checkHit(number);
 			$(this).attr("src", image);
 				ifSank();
 				});
 	$('.11').click(function(){
+			$('.11').unbind("click");
 				var number = 11;
 				checkHit(number);
 			$(this).attr("src", image);
 				ifSank();
 				});
 	$('.12').click(function(){
+			$('.12').unbind("click");
 				var number = 12;
 				checkHit(number);
 			$(this).attr("src", image);
 				ifSank();
 				});
 	$('.13').click(function(){
+			$('.13').unbind("click");
 				var number = 13;
 				checkHit(number);
 			$(this).attr("src", image);
 				ifSank();
 				});
 	$('.14').click(function(){
+			$('.14').unbind("click");
 				var number = 14;
 				checkHit(number);
 			$(this).attr("src", image);
 				ifSank();
 				});
 	$('.15').click(function(){
+				$('.15').unbind("click");
 				var number = 15;
 				checkHit(number);
 			$(this).attr("src", image);
 				ifSank();
 				});
 	$('.16').click(function(){
+				$('.16').unbind("click");
 				var number = 16;
 				checkHit(number);
 			$(this).attr("src", image);
@@ -117,24 +133,29 @@ function increaseCount(count) {
 
 	myArray = [];
 	var number ;
-	for (var i = 0; i < 4 ; i++){
-		number = Math.floor(Math.random() * 16) + 1; 
-		if (myArray.indexOf(number) < 0){
-			myArray.push(number);
-		} else {
-			number = Math.floor(Math.random() * 16) + 1;
-		if (myArray.indexOf(number) < 0){
-			myArray.push(number);
-		} 
+		do { number = Math.floor(Math.random() * 16) + 1; 
+			if (myArray.indexOf(number) < 0){
+				myArray.push(number);
+			}
+			console.log(myArray);
 		}
-		console.log(myArray);
-	}
+		while (myArray.length < 4);
+		
+		
+		
+	
 
 	function checkHit(number){
 		if (myArray.indexOf(number) > -1){
 			image = "img/hit.png";
+			arr = myArray.filter(function(item){
+				if(item !== number){
+					return item;
+				}
+			});
 			sank += 1;
-		} else {
+			myArray = arr;
+		} else {	
 		image = "img/miss.jpg";
 		}
 		return image;
